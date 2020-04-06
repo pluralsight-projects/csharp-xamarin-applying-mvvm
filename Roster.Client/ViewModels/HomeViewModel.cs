11a57f5e79b7a56128c4795c2e6b32662a2a5eea
Sidney Andrews <sidney@seesharprun.net>
2020-04-06 11:37:31 -0400

    Mod 04 Solution

---
+++ Roster.Client/ViewModels/HomeViewModel.cs
@@ -0,0 +1,9 @@
+using System.ComponentModel;
+
+namespace Roster.Client.ViewModels
+{
+    public partial class HomeViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
